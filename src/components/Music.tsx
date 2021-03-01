b9525331e58182b608efab8c50c0f780ccff8492
mmmommm <ryota1839@gmail.com>
2021-03-01 10:09:35 +0900

    fix: bg color & font color

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Link from "next/link"
-import styles from "../styles/components/Artist.module.scss"
+import styles from "../styles/components/Music.module.scss"
 import { Music } from "@types"
 
 export const MusicInfo: React.FC<{ music: Music }> = ({ music }) => {
   return (
-    <div className={styles.artist_card}>
+    <div className={styles.music_card}>
       <Link href={music.name} passHref>
         <>
           <img src={music.img} alt="artist image"/>
