e08ca0ac65385f44b88c2072200f0478f168a4bf
mmmommm <ryota1839@gmail.com>
2021-04-10 16:41:19 +0900

    add: routing to change language

--- frontend/src/pages/Language.tsx
+++ frontend/src/pages/Language.tsx
@@ -1,15 +1,20 @@
+import Link from "next/link";
 import React from "react";
 import styles from "../styles/pages/Language.module.scss";
 
 const Language: React.FC = () => {
   return (
     <div className={styles.languages}>
-      <button>
-        Japanese
-      </button>
-      <button>
-        English
-      </button>
+      <div>
+        <Link href="/Language" locale="ja">
+          Japanese
+        </Link>
+      </div>
+      <div>
+        <Link href="/Language" locale="en-US">
+          English
+        </Link>
+      </div>
     </div>
   );
 };
